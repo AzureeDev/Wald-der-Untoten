9351659ef2edcdc561e7f0ad7ef676b9ae917ed3
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2018-05-14 14:05:12 -0700

    File cleanup + shovel icon
    -Removed unused things
    -:shovel: icon

--- hooks/MeleeWeaponsTweakData.lua
+++ hooks/MeleeWeaponsTweakData.lua
@@ -3,6 +3,7 @@ Hooks:PostHook(BlackMarketTweakData, "_init_melee_weapons", "zm_init_new_melee_w
     local data = table.merge(deep_clone(self.melee_weapons["shovel"]), table.merge({
         name_id = "wpn_melee_zdann",
         dlc = "infamy",
+        texture_bundle_folder = "kurumi",
         custom = true,
         unit = "units/mods/weapons/wpn_fps_mel_zdann/wpn_fps_mel_zdann",
         third_unit = "units/mods/weapons/wpn_third_mel_zdann/wpn_third_mel_zdann",
