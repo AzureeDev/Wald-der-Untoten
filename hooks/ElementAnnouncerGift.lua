86300ed5634ad2c54759c7f390a6f091382f4b9b
Nepgearsy <timertimer5@gmail.com>
2018-05-21 21:16:07 +0200

    soft_replenish

--- hooks/ElementAnnouncerGift.lua
+++ hooks/ElementAnnouncerGift.lua
@@ -21,7 +21,7 @@ function ElementAnnouncerGift:on_executed(instigator)
         if player_unit then
             for id,weapon in pairs( player_unit:inventory():available_selections() ) do
                 if alive(weapon.unit) then
-                    weapon.unit:base():replenish()
+                    weapon.unit:base():soft_replenish()
                     managers.hud:set_ammo_amount( id, weapon.unit:base():ammo_info() )
                 end
                 
