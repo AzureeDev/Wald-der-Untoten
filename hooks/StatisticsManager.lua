220807d51a425ea83fbe7d23ab0462e48fc24fc0
Nepgearsy <timertimer5@gmail.com>
2018-05-31 10:42:04 +0200

    Voice line on kill

--- hooks/StatisticsManager.lua
+++ hooks/StatisticsManager.lua
@@ -18,4 +18,12 @@ Hooks:PostHook(StatisticsManager, "killed", "zm_init_add_zmpoints", function(sel
 
         managers.wdu:_add_money_to(peer_id, points_to_add)
     end
+
+    if alive(managers.player:player_unit()) then
+        local random = math.random(0, 100)
+
+        if random <= 6 then
+            managers.player:player_unit():sound():say("g24", true, true)
+        end
+    end
 end)
