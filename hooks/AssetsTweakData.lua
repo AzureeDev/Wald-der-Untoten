d0485124ecb3c9f8401873d942193568a69386db
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2018-05-31 18:22:52 -0700

    Added additional asset to explain Deadshot
    Texture is probably a placeholder because I suck at making a torn paper effect

--- hooks/AssetsTweakData.lua
+++ hooks/AssetsTweakData.lua
@@ -5,4 +5,10 @@ Hooks:PostHook(AssetsTweakData, "_init_assets", "zm_init_assets", function(self,
 		stages = {"zm_the_forest"},
 		no_mystery = true
 	}
+    self.zm_wald_der_untoten_perks_2 = {
+		name_id = "zm_wald_der_untoten_perks_2_name",
+		texture = "map_assets/perks_2",
+		stages = {"zm_the_forest"},
+		no_mystery = true
+	}
 end)
