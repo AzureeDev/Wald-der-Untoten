e845ffabf828cc62a2f3f680d1cce1a524b570f1
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2018-06-01 16:04:40 -0700

    Assets tweak
    Removed Risk Level and Gage Package assets

--- hooks/AssetsTweakData.lua
+++ hooks/AssetsTweakData.lua
@@ -12,3 +12,17 @@ Hooks:PostHook(AssetsTweakData, "_init_assets", "zm_init_assets", function(self,
 		no_mystery = true
 	}
 end)
+
+Hooks:PostHook(AssetsTweakData, "_init_risk_assets", "zm_ignore_risk_asset", function(self, tweak_data)
+    table.insert(self.risk_pd.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_swat.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_fbi.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_death_squad.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_easy_wish.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_death_wish.exclude_stages, "zm_the_forest")
+    table.insert(self.risk_sm_wish.exclude_stages, "zm_the_forest")
+end)
+
+Hooks:PostHook(AssetsTweakData, "_init_gage_assets", "zm_ignore_gage_asset", function(self, tweak_data)
+    table.insert(self.gage_assignment.exclude_stages, "zm_the_forest")
+end)
