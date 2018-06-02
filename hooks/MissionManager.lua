5332554b05ac43799d71893147993e6469fe0acd
Nepgearsy <timertimer5@gmail.com>
2018-06-02 16:00:10 +0200

    (no title)

--- hooks/MissionManager.lua
+++ hooks/MissionManager.lua
@@ -1,6 +1,8 @@
-dofile(ModPath .. "hooks/ElementWeaponSwitch.lua")
-dofile(ModPath .. "hooks/ElementPointChecker.lua")
-dofile(ModPath .. "hooks/ElementWave.lua")
-dofile(ModPath .. "hooks/ElementAnnouncerGift.lua")
-dofile(ModPath .. "hooks/ElementDynamicEnv.lua")
-dofile(ModPath .. "hooks/ElementPlayVideo.lua")
+local Path = WDU.ModPath
+
+dofile(Path .. "hooks/ElementWeaponSwitch.lua")
+dofile(Path .. "hooks/ElementPointChecker.lua")
+dofile(Path .. "hooks/ElementWave.lua")
+dofile(Path .. "hooks/ElementAnnouncerGift.lua")
+dofile(Path .. "hooks/ElementDynamicEnv.lua")
+dofile(Path .. "hooks/ElementPlayVideo.lua")
