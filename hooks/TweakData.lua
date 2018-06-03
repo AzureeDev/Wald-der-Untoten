4c1472bbd60010f721f0eb80aeb0548e766d2976
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2018-06-02 18:50:42 -0700

    Removed unnecessary level check
    :shovel:

--- hooks/TweakData.lua
+++ hooks/TweakData.lua
@@ -1,5 +1,3 @@
-if Global.level_data.level_id == "zm_the_forest" then
-    tweak_data.projectiles.launcher_frag_arbiter.damage = 150
-
-    tweak_data.projectiles.fir_com.damage = 15
-end
+ --:owoshovel:
+	tweak_data.projectiles.launcher_frag_arbiter.damage = 150	--M2018 Neru
+	tweak_data.projectiles.fir_com.damage = 15	--Incendiary Grenade
