19acc297ec5d29a91d77ac0ead12b6dffbb768d9
Nepgearsy <timertimer5@gmail.com>
2018-06-09 14:11:47 +0200

    Civ damage force fix

---
+++ hooks/CivilianDamage.lua
@@ -0,0 +1,19 @@
+function CivilianDamage:damage_bullet(attack_data)
+    return
+end
+
+function CivilianDamage:damage_explosion(attack_data)
+    return
+end
+
+function CivilianDamage:damage_fire(attack_data)
+    return
+end
+
+function CivilianDamage:damage_melee(attack_data)
+    return
+end
+
+function CivilianDamage:damage_tase(attack_data)
+    return
+end
