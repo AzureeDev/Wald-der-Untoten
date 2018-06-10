2b4a0d6dc87a0ba01c0d55dfbb406fc4ea9c382f
Lolnoob1 <12839830+Lolnoobwut1@users.noreply.github.com>
2018-06-10 10:56:06 -0700

    Friendly fire changes -Increased M2018 Neru friendly fire damage to 100 (from 80) -Decreased RPG friendly fire damage to 100 (from 400)

--- hooks/TweakData.lua
+++ hooks/TweakData.lua
@@ -1,3 +1,5 @@
  --:owoshovel:
 	tweak_data.projectiles.launcher_frag_arbiter.damage = 600	--M2018 Neru
+	tweak_data.projectiles.launcher_frag_arbiter.player_damage = 10	--M2018 Neru player damage
+	tweak_data.projectiles.launcher_rocket.player_damage = 10	--RPG player damage
 	tweak_data.projectiles.fir_com.damage = 15	--Incendiary Grenade
